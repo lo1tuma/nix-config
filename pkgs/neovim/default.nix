9fa0da5d239bd47eba718b32134f76357cea0786
Mathias Schreck <schreck.mathias@googlemail.com>
2019-01-07 11:12:12 +0100

    Rename pythonPackages.neovim to pynvim

--- pkgs/neovim/default.nix
+++ pkgs/neovim/default.nix
@@ -3,5 +3,5 @@
 pkgs.neovim.override {
   vimAlias = true;
   configure = import ./config.nix { inherit pkgs; };
-  extraPython3Packages = pythonPackages: [ pythonPackages.neovim ];
+  extraPython3Packages = pythonPackages: [ pythonPackages.pynvim ];
 }
