0519677424902a336fe757bd97cfad4db2416160
Mathias Schreck <mathias.schreck@joyn.de>
2022-09-01 14:58:44 +0200

    Extract plugins array

--- pkgs/neovim/config.nix
+++ pkgs/neovim/config.nix
@@ -1,38 +1,39 @@
 { pkgs }:
 
 let
-  vimrc = pkgs.callPackage ./vimrc.nix {};
+    vimrc = pkgs.callPackage ./vimrc.nix {};
+    plugins = [
+        pkgs.vimPlugins.base16-vim
+        pkgs.vimPlugins.echodoc
+        pkgs.vimPlugins.vim-json
+        pkgs.vimPlugins.tabular
+        pkgs.vimPlugins.vim-markdown
+        pkgs.vimPlugins.vim-nix
+        pkgs.vimPlugins.typescript-vim
+        pkgs.vimPlugins.vim-javascript
+        pkgs.vimPlugins.vim-nix
+        pkgs.vimPlugins.editorconfig-vim
+        pkgs.vimPlugins.vim-surround
+        pkgs.vimPlugins.vim-repeat
+        pkgs.vimPlugins.vim-gitgutter
+        pkgs.vimPlugins.coc-nvim
+        pkgs.vimPlugins.coc-rls
+        pkgs.vimPlugins.coc-yaml
+        pkgs.vimPlugins.coc-neco
+        pkgs.vimPlugins.coc-json
+        pkgs.vimPlugins.coc-lists
+        pkgs.vimPlugins.coc-eslint
+        pkgs.vimPlugins.coc-tsserver
+        pkgs.vimPlugins.coc-prettier
+        pkgs.vimPlugins.vim-toml
+        pkgs.vimPlugins.telescope-nvim
+        pkgs.vimPlugins.popup-nvim
+        pkgs.vimPlugins.plenary-nvim
+        pkgs.vimPlugins.nvim-treesitter
+        pkgs.vimPlugins.nvim-web-devicons
+    ];
 in {
-  customRC = vimrc;
-  packages.nix.start = [
-    pkgs.vimPlugins.vim-json
-    pkgs.vimPlugins.tabular
-    pkgs.vimPlugins.vim-markdown
-    pkgs.vimPlugins.vim-nix
-    pkgs.vimPlugins.vim-javascript
-    pkgs.vimPlugins.vim-nix
-    pkgs.vimPlugins.typescript-vim
-    pkgs.vimPlugins.editorconfig-vim
-    pkgs.vimPlugins.vim-surround
-    pkgs.vimPlugins.vim-repeat
-    pkgs.vimPlugins.vim-gitgutter
-    pkgs.vimPlugins.coc-nvim
-    pkgs.vimPlugins.coc-rls
-    pkgs.vimPlugins.coc-yaml
-    pkgs.vimPlugins.coc-neco
-    pkgs.vimPlugins.coc-json
-    pkgs.vimPlugins.coc-lists
-    pkgs.vimPlugins.coc-eslint
-    pkgs.vimPlugins.coc-tsserver
-    pkgs.vimPlugins.coc-prettier
-    pkgs.vimPlugins.echodoc
-    pkgs.vimPlugins.base16-vim
-    pkgs.vimPlugins.vim-toml
-    pkgs.vimPlugins.telescope-nvim
-    pkgs.vimPlugins.popup-nvim
-    pkgs.vimPlugins.plenary-nvim
-    pkgs.vimPlugins.nvim-treesitter
-    pkgs.vimPlugins.nvim-web-devicons
-  ];
+    customRC = vimrc;
+    packages.myPlugins.start = plugins;
 }
 
