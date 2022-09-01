61f46d9e64d679ef5e6fa7fb8d9f4b58693c9b15
Mathias Schreck <mathias.schreck@joyn.de>
2022-09-01 13:02:36 +0200

    Stop using vam

--- pkgs/neovim/config.nix
+++ pkgs/neovim/config.nix
@@ -4,38 +4,35 @@ let
   vimrc = pkgs.callPackage ./vimrc.nix {};
 in {
   customRC = vimrc;
-  vam = {
-    knownPlugins = pkgs.vimPlugins;
-
-    pluginDictionaries = [
-      { name = "vim-json"; } # elzr/vim-json
-      { name = "tabular"; } # godlygeek/tabular
-      { name = "vim-markdown"; } # plasticboy/vim-markdown
-      { name = "vim-nix"; } # LnL7/vim-nix
-      { name = "vim-javascript"; } # pangloss/vim-javascript
-      { name = "typescript-vim"; } # leafgarland/typescript-vim
-      { name = "editorconfig-vim"; } # editorconfig/editorconfig-vim
-      { name = "vim-surround"; } # tpope/vim-surround
-      { name = "vim-repeat"; } # tpope/vim-repeat
-      { name = "vim-gitgutter"; } # airblade/vim-gitgutter
-      { name = "coc-nvim"; }
-      { name = "coc-rls"; }
-      { name = "coc-yaml"; }
-      { name = "coc-neco"; }
-      { name = "coc-json"; }
-      { name = "coc-lists"; }
-      { name = "coc-eslint"; }
-      { name = "coc-tsserver"; }
-      { name = "coc-prettier"; }
-      { name = "echodoc"; }
-      { name = "base16-vim"; }
-      { name = "vim-toml"; }
-      { name = "telescope-nvim"; }
-      { name = "popup-nvim"; } # required by telescope
-      { name = "plenary-nvim"; } # required by telescope
-      { name = "nvim-treesitter"; } # require by telescope
-      { name = "nvim-web-devicons"; }
-    ];
-  };
+  packages.nix.start = [
+    pkgs.vimPlugins.vim-json
+    pkgs.vimPlugins.tabular
+    pkgs.vimPlugins.vim-markdown
+    pkgs.vimPlugins.vim-nix
+    pkgs.vimPlugins.vim-javascript
+    pkgs.vimPlugins.vim-nix
+    pkgs.vimPlugins.typescript-vim
+    pkgs.vimPlugins.editorconfig-vim
+    pkgs.vimPlugins.vim-surround
+    pkgs.vimPlugins.vim-repeat
+    pkgs.vimPlugins.vim-gitgutter
+    pkgs.vimPlugins.coc-nvim
+    pkgs.vimPlugins.coc-rls
+    pkgs.vimPlugins.coc-yaml
+    pkgs.vimPlugins.coc-neco
+    pkgs.vimPlugins.coc-json
+    pkgs.vimPlugins.coc-lists
+    pkgs.vimPlugins.coc-eslint
+    pkgs.vimPlugins.coc-tsserver
+    pkgs.vimPlugins.coc-prettier
+    pkgs.vimPlugins.echodoc
+    pkgs.vimPlugins.base16-vim
+    pkgs.vimPlugins.vim-toml
+    pkgs.vimPlugins.telescope-nvim
+    pkgs.vimPlugins.popup-nvim
+    pkgs.vimPlugins.plenary-nvim
+    pkgs.vimPlugins.nvim-treesitter
+    pkgs.vimPlugins.nvim-web-devicons
+  ];
 }
 
