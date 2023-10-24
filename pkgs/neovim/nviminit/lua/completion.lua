d2ed7643fa813a3afa9d09c64353eb9f14431180
Mathias Schreck <schreck.mathias@gmail.com>
2023-10-24 17:59:42 +0200

    Don’t preselect completion item

--- pkgs/neovim/nviminit/lua/completion.lua
+++ pkgs/neovim/nviminit/lua/completion.lua
@@ -1,6 +1,7 @@
 local cmp = require("cmp")
 
 cmp.setup({
+    preselect = cmp.PreselectMode.None,
     sources = cmp.config.sources({
         { name = "nvim_lsp" },
         { name = "nvim_lsp_document_symbol" },
@@ -14,6 +15,6 @@ cmp.setup({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        ["<CR>"] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
     }),
 })
