011ab13c540b920e96d19bf076fb9eff6d0fd624
Mathias Schreck <mathias.schreck@misterspex.de>
2024-09-17 22:19:04 +0200

    Update whichKey setup

--- pkgs/neovim/nviminit/lua/help.lua
+++ pkgs/neovim/nviminit/lua/help.lua
@@ -1,6 +1,18 @@
 local whichKey = require("which-key")
 
 whichKey.setup({
+    preset = "modern",
+    delay = function(ctx)
+        return ctx.plugin and 0 or 500
+    end,
+    filter = function(mapping)
+      return true
+      end,
+      spec = {},
+      notify = true,
+      defer = function(ctx)
+    return ctx.mode == "V" or ctx.mode == "<C-V>"
+  end,
     plugins = {
         marks = false, -- shows a list of your marks on ' and `
         registers = false, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
@@ -20,142 +32,125 @@ whichKey.setup({
             g = true, -- bindings for prefixed with g
         },
     },
-    -- add operators that will trigger motion and text object completion
-    -- to enable all native operators, set the preset / operators plugin above
-    operators = { gc = "Comments" },
-    key_labels = {
-        -- override the label used to display some keys. It doesn't effect WK in any other way.
-        -- For example:
-        -- ["<space>"] = "SPC",
-        -- ["<cr>"] = "RET",
-        -- ["<tab>"] = "TAB",
-    },
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
         separator = "➜", -- symbol used between a key and it's label
         group = "+", -- symbol prepended to a group
     },
-    popup_mappings = {
-        scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
-    },
-    window = {
-        border = "none", -- none, single, double, shadow
-        position = "bottom", -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-        winblend = 0,
+    triggers = {
+        { "<auto>", mode = "nixsotc" },
     },
     layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
+        width = { min = 20 },
         spacing = 3, -- spacing between columns
-        align = "left", -- align columns left, center or right
     },
-    ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-    hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+    keys = {
+        scroll_down = "<c-d>", -- binding to scroll down inside the popup
+        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+      },
+      sort = { "local", "order", "group", "alphanum", "mod" },
+      expand = 0, -- expand groups when <= n mappings
     show_help = true, -- show help message on the command line when the popup is visible
-    triggers = "auto", -- automatically setup triggers
-    -- triggers = {"<leader>"} -- or specify a list manually
-    triggers_blacklist = {
-        -- list of mode / prefixes that should never be hooked by WhichKey
-        -- this is mostly relevant for key maps that start with a native binding
-        -- most people should not need to change this
-        i = { "j", "k" },
-        v = { "j", "k" },
-    },
+    show_keys = true,
+    debug = false
 })
 
 local telescope = require("telescope.builtin")
 
-whichKey.register({
-    h = {
-        name = "Git Hunk",
-        p = { "Preview" },
-        s = { "Stage" },
-        u = { "Undo / Revert" },
+whichKey.add({
+    { "<leader>f", group = "Find" },
+    { "<leader>fb", telescope.buffers, desc = "Open Buffers" },
+    { "<leader>ff", telescope.find_files, desc = "Find File (by name)" },
+    { "<leader>fg", telescope.live_grep, desc = "Find File (by content)" },
+    { "<leader>fh", telescope.help_tags, desc = "Open Help Entries" },
+    { "<leader>fr", telescope.oldfiles, desc = "Open Recent File" },
+    { "<leader>h", group = "Git Hunk" },
+    { "<leader>hp", desc = "Preview" },
+    { "<leader>hs", desc = "Stage" },
+    { "<leader>hu", desc = "Undo / Revert" },
+    { "<leader>l", group = "LSP" },
+    {
+        "<leader>lD",
+        function()
+            vim.lsp.buf.declaration()
+        end,
+        desc = "Declaration",
     },
-    f = {
-        name = "Find",
-        f = { telescope.find_files, "Find File (by name)" },
-        g = { telescope.live_grep, "Find File (by content)" },
-        b = { telescope.buffers, "Open Buffers" },
-        r = { telescope.oldfiles, "Open Recent File" },
-        h = { telescope.help_tags, "Open Help Entries" },
+    {
+        "<leader>lR",
+        function()
+            vim.lsp.buf.rename()
+        end,
+        desc = "Rename",
     },
-    l = {
-        name = "LSP",
-        d = {
-            function()
-                telescope.diagnostics()
-            end,
-            "File Diagnostics",
-        },
-        a = {
-            function()
-                vim.lsp.buf.code_action()
-            end,
-            "Code Action",
-        },
-        h = {
-            function()
-                vim.lsp.buf.hover()
-            end,
-            "Hover",
-        },
-        f = {
-            function()
-                vim.lsp.buf.format()
-            end,
-            "Format",
-        },
-        r = {
-            function()
-                vim.lsp.buf.references()
-            end,
-            "references",
-        },
-        R = {
-            function()
-                vim.lsp.buf.rename()
-            end,
-            "Rename",
-        },
-        s = {
-            function()
-                vim.lsp.buf.signature_help()
-            end,
-            "Singnature help",
-        },
-        c = {
-            function()
-                vim.lsp.buf.definition()
-            end,
-            "Code Definition",
-        },
-        t = {
-            function()
-                vim.lsp.buf.type_definition()
-            end,
-            "Type Definition",
-        },
-        D = {
-            function()
-                vim.lsp.buf.declaration()
-            end,
-            "Declaration",
-        },
-        i = {
-            function()
-                vim.lsp.buf.implementation()
-            end,
-            "Implementation",
-        },
-        q = {
-            function()
-                vim.diagnostic.setloclist()
-            end,
-            "Quickfix",
-        },
+    {
+        "<leader>la",
+        function()
+            vim.lsp.buf.code_action()
+        end,
+        desc = "Code Action",
     },
-}, { prefix = "<leader>" })
+    {
+        "<leader>lc",
+        function()
+            vim.lsp.buf.definition()
+        end,
+        desc = "Code Definition",
+    },
+    {
+        "<leader>ld",
+        function()
+            telescope.diagnostics()
+        end,
+        desc = "File Diagnostics",
+    },
+    {
+        "<leader>lf",
+        function()
+            vim.lsp.buf.format()
+        end,
+        desc = "Format",
+    },
+    {
+        "<leader>lh",
+        function()
+            vim.lsp.buf.hover()
+        end,
+        desc = "Hover",
+    },
+    {
+        "<leader>li",
+        function()
+            vim.lsp.buf.implementation()
+        end,
+        desc = "Implementation",
+    },
+    {
+        "<leader>lq",
+        function()
+            vim.diagnostic.setloclist()
+        end,
+        desc = "Quickfix",
+    },
+    {
+        "<leader>lr",
+        function()
+            vim.lsp.buf.references()
+        end,
+        desc = "references",
+    },
+    {
+        "<leader>ls",
+        function()
+            vim.lsp.buf.signature_help()
+        end,
+        desc = "Singnature help",
+    },
+    {
+        "<leader>lt",
+        function()
+            vim.lsp.buf.type_definition()
+        end,
+        desc = "Type Definition",
+    },
+})
