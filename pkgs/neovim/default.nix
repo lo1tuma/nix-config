df3d0fad30df12806609dce849a3adc09311fc22
Mathias Schreck <schreck.mathias@gmail.com>
2026-04-27 03:06:56 +0200

    Modernize neovim wrapper setup

--- pkgs/neovim/default.nix
+++ pkgs/neovim/default.nix
@@ -1,33 +1,45 @@
 { pkgs }:
 let
-    neovimConfig = pkgs.neovimUtils.makeNeovimConfig (import ./config.nix { inherit pkgs; });
-    extraRuntimeDependencies = [
-        pkgs.vscode-langservers-extracted
-        pkgs.nodePackages.cspell
-        pkgs.nodePackages.typescript-language-server
-        pkgs.lua-language-server
-        pkgs.nil
-        pkgs.stylua
-        pkgs.shfmt
-        pkgs.yaml-language-server
-    ];
-    extraMakeWrapperArgs = "--prefix PATH : '${pkgs.lib.makeBinPath extraRuntimeDependencies}'";
-in pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (neovimConfig // {
-  luaRcContent = let
-        inherit (builtins) concatStringsSep readFile map;
+  config = import ./config.nix { inherit pkgs; };
+  extraRuntimeDependencies = [
+    pkgs.vscode-langservers-extracted
+    pkgs.cspell
+    pkgs.typescript-language-server
+    pkgs.lua-language-server
+    pkgs.nil
+    pkgs.stylua
+    pkgs.shfmt
+    pkgs.yaml-language-server
+  ];
+  extraMakeWrapperArgs = "--prefix PATH : '${pkgs.lib.makeBinPath extraRuntimeDependencies}'";
+  luaRcContent =
+    let
+      inherit (builtins) concatStringsSep map readFile;
 
-        sources = [
-          ./nviminit/lua/base-keymap.lua
-          ./nviminit/lua/nviminit.lua
-          ./nviminit/lua/syntax.lua
-          ./nviminit/lua/telescope-settings.lua
-          ./nviminit/lua/help.lua
-          ./nviminit/lua/linter.lua
-          ./nviminit/lua/lsp.lua
-          ./nviminit/lua/formatter.lua
-          ./nviminit/lua/completion.lua
-        ];
-      in concatStringsSep "\n" (map readFile sources);
-  wrapperArgs = pkgs.lib.escapeShellArgs neovimConfig.wrapperArgs + " " + extraMakeWrapperArgs;
-  wrapRc = true;
-})
+      sources = [
+        ./nviminit/lua/base-keymap.lua
+        ./nviminit/lua/nviminit.lua
+        ./nviminit/lua/syntax.lua
+        ./nviminit/lua/telescope-settings.lua
+        ./nviminit/lua/help.lua
+        ./nviminit/lua/linter.lua
+        ./nviminit/lua/lsp.lua
+        ./nviminit/lua/formatter.lua
+        ./nviminit/lua/completion.lua
+      ];
+    in
+    concatStringsSep "\n" (map readFile sources);
+in
+pkgs.wrapNeovim pkgs.neovim-unwrapped {
+  inherit extraMakeWrapperArgs;
+  inherit (config) vimAlias withPython3 extraPython3Packages withNodeJs;
+
+  configure = {
+    customRC = ''
+      lua << EOF
+      ${luaRcContent}
+      EOF
+    '';
+    packages.myNeovimPackage.start = config.plugins;
+  };
+}
